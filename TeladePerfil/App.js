92c3b1e1eace952b6ecd7cac859b912c9b844553
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2022-06-18 21:40:19 -0300

    StyleSheet

--- TeladePerfil/App.js
+++ TeladePerfil/App.js
@@ -4,17 +4,24 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>TELA DE PROFILE</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+      flex: 1,
+      backgroundColor: 'grey',
+      flex: 1,
+    },
+
+
+    text: {
+          fontSize: 50,
+          fontWeight: 'bold',
+          color: 'black',     
+    },
+
 });
