8f043e7b03bacfb6688ba2b39422e55f7613701f
Gleiciane Galdino <gleicianegaldino25@gmail.com>
2022-06-19 11:51:09 -0300

    Imagem

--- TeladePerfil/App.js
+++ TeladePerfil/App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Image, Text, View, SafeAreaView } from 'react-native';
+
+
+const colorGithub = '#010409';
+const imageGithub = 'https://avatars.githubusercontent.com/u/78940661?s=400&u=dbf6b4edb4f6ac84fb95ce7488afac811fa104d2&v=4';
+
 
 export default function App() {
   return (
 
     <SafeAreaView style={styles.container}>
       <View >
-        <StatusBar backgroundColor={"grey"} barStyle="dark-content"/>
+        <StatusBar style="auto" backgroundColor={colorGithub} barStyle="light-content"/>
+
         <Text style={styles.text}>TELA DE PROFILE</Text>
-        <StatusBar style="auto"/>
+        <Image accessibilitylabel='Gleiciane foto de perfil com fundo branco' 
+        style={styles.avatar} source={{uri:imageGithub}}/>
+
+       
       </View>
     </SafeAreaView>
   );
@@ -17,7 +26,7 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: 'grey',
+      backgroundColor: colorGithub,
       flex: 1,
     },
 
@@ -26,6 +35,15 @@ const styles = StyleSheet.create({
           fontSize: 50,
           fontWeight: 'bold',
           color: 'black',     
+    },
+
+
+    avatar:{
+      height: 200,
+      width: 200,
+      borderRadius:100,
+      borderColor: 'width',
+      borderWidth: 2,
     },
 
 });
